ca2bd9d16e51956267f7a4c867832cd5d1666fd7
LeydiH <131090822+LeydiH@users.noreply.github.com>
2024-01-13 01:22:20 +0100

    W01 Activity: Course Home Page Development

---
+++ home page w01/scripts/getDates.js
@@ -0,0 +1,6 @@
+// Display copyright year and last modification date
+document.getElementById('footerText').innerText = `Copyright ${new Date().getFullYear()} Leydi M Hernandez - Toledo, Spain`;
+
+// Display last modification date Last Modified: ${document.lastModified}
+document.getElementById('lastModified').innerText = `Last Modified: ${document.lastModified}`;
+
