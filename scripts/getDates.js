624a84f1d82caf9526833cd30ab04c35395422ed
LeydiH <131090822+LeydiH@users.noreply.github.com>
2024-01-26 00:06:35 +0100

    W03 Team Activity Home Page-Responsive Images

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,6 +1,30 @@
-// Display copyright year and last modification date
-document.getElementById('footerText').innerText = `Copyright ${new Date().getFullYear()} Leydi M Hernandez - Toledo, Spain`;
 
-// Display last modification date Last Modified: ${document.lastModified}
-document.getElementById('lastModified').innerText = `Last Modified: ${document.lastModified}`;
+const navElement = document.querySelector('#nav');
+const hambutton = document.querySelector('#menu');
 
+hambutton.addEventListener('click', () => {
+	navElement.classList.toggle('hide');
+	hambutton.classList.toggle('show');
+});
+
+console.log(document.lastModified)
+document.querySelector('#lastModified').innerHTML = document.lastModified;
+document.addEventListener('DOMContentLoaded', function () {
+	const modeToggleBtn = document.getElementById('modeToggleBtn');
+	const body = document.body;
+
+	modeToggleBtn.addEventListener('click', function () {
+		body.classList.toggle('dark-mode');
+	});
+});
+
+const visitsDisplay = document.querySelector("#visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+if (numVisits !== null) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit to our site.`;
+}
+
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
