4113d5759265f2ebc1c204cb2da647c34f8cab77
LeydiH <131090822+LeydiH@users.noreply.github.com>
2024-02-08 19:07:26 +0100

    W05 The Chamber Directory Page

---
+++ chamber/scripts/directory-json.js
@@ -0,0 +1,57 @@
+const url = 'https://raw.githubusercontent.com/LeydiH/wdd230/7fe8e18b145c13905b1a7e4e0284090297e824ba/chamber/members.json'; // Path for online data.json file
+
+// LOCAL JSON FOR TESTING: const url = 'data.json';
+
+async function getCompanyData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayCompanies(data.companies);
+}
+
+const displayCompanies = (companies) => {
+    const directory = document.querySelector('.directory');
+
+    companies.forEach((company) => {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let website = document.createElement('a');
+        let icon = document.createElement('img');
+
+        h2.textContent = company.name;
+        address.textContent = `Address: ${company.address}`;
+        phone.textContent = `Phone: ${company.phone}`;
+        website.href = company.website;
+        website.textContent = 'Visit Website';
+        icon.setAttribute('src', company.icon);
+
+
+        card.appendChild(h2);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+        card.appendChild(icon);
+
+        directory.appendChild(card);
+    });
+}
+
+getCompanyData();
+
+// switch button to change list view of directory
+
+document.getElementById('switchViewBtn').addEventListener('click', function() {
+    var directory = document.getElementById('directory');
+    
+    if (directory.classList.contains('grid')) {
+        directory.classList.remove('grid');
+        directory.classList.add('list');
+        this.innerText = 'Switch to Grid View';
+    } else {
+        directory.classList.remove('list');
+        directory.classList.add('grid');
+        this.innerText = 'Switch to List View';
+    }
+  });
+
