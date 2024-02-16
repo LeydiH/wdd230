9c01b0a57700c61f23c073b5c326c815f4af9b17
LeydiH <131090822+LeydiH@users.noreply.github.com>
2024-02-16 17:59:40 +0100

    W06 Chamber Home Page Enhancements

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -23,3 +23,33 @@ if (currentDay === 1 || currentDay === 2) { // 1 is Monday, 2 is Tuesday
 } else {
     document.getElementById('banner').style.display = "none"; // hide banner
 }
+
+// companies.json
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+    }
+}
+
+fetch('companies.json')
+    .then(response => response.json())
+    .then(data => {
+        shuffleArray(data.companies);
+
+        for (let i = 0; i < 3; i++) {
+            const spotlight = document.querySelector(`.spotlight${i+1}`);
+            const company = data.companies[i];
+
+            spotlight.innerHTML += `<p><strong>Our top members:</strong> ${company.name} - <strong>Website:</strong> <a href="${company.website}">${company.website}</a></p>`;
+            spotlight.innerHTML += `<img src="${company.icon}" alt="${company.name}">`;
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+
+
+// companies.json END
+
